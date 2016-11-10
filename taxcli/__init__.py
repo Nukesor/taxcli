b471c1253cd18b16b7eddd55fca21ffe0f87c031
Arne Beer <arne@twobeer.de>
2016-11-10 02:55:03 +0100

    Binary for invoice files

--- taxcli/__init__.py
+++ taxcli/__init__.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 
-engine = create_engine("postgresql://localhost/taxcli", echo=True)
+engine = create_engine("postgresql://localhost/taxcli")
 base = declarative_base(bind=engine)
 
 import taxcli.models
--- taxcli/models/invoice.py
+++ taxcli/models/invoice.py
@@ -3,6 +3,7 @@ from taxcli import base
 
 from sqlalchemy import (
     Column,
+    CheckConstraint,
     ForeignKeyConstraint,
     PrimaryKeyConstraint,
 )
@@ -11,6 +12,7 @@ from sqlalchemy.types import (
     Date,
     Enum,
     Integer,
+    LargeBinary,
     String,
 )
 
@@ -25,6 +27,8 @@ class Invoice(base):
     __table_args__ = (
         ForeignKeyConstraint(['contact_alias'], ['contacts.alias']),
         PrimaryKeyConstraint('invoice_number', 'contact_alias'),
+        CheckConstraint('invoice_file is null and invoice_file_type is null or '
+                        'invoice_file is not null and invoice_file_type is not null'),
     )
 
     invoice_number = Column(String(100), nullable=False)
@@ -33,3 +37,5 @@ class Invoice(base):
     afa = Column(Integer)
     date = Column(Date, nullable=False)
     invoice_type = Column(Enum(InvoiceTypes))
+    invoice_file = Column(LargeBinary)
+    invoice_file_type = Column(String(10))
